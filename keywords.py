e393fbd21ef249ed3cb8e6b8fe615b1d00e7ac77
jitendra singhal <jiten.cs2010@gmail.com>
2019-10-10 11:07:49 +0530

    keywords

---
+++ keywords.py
@@ -0,0 +1,18 @@
+import keyword
+keyword.kwlist
+
+keyword.iskeyword('try')
+
+'try'.isidentifier()
+
+test = 10
+id(test)
+test = 10
+id(test)
+test = 11
+id(test)
+
+type(test)
+
+test = 'char'
+type(test)
